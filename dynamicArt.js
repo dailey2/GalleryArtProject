5e44ef52c91c297444d398d5d4c18f8f6d17eccc
dailey2 <98828394+dailey2@users.noreply.github.com>
2023-05-07 13:23:55 -0400

    Update dynamicArt.js

--- dynamicArt.js
+++ dynamicArt.js
@@ -1,56 +1,48 @@
-var baseUrl = 'http://3.15.139.27:5005';
-const url = "http://localhost:5005/dynamicArt/getEmotionPercentages";
+const baseUrl = 'http://3.15.139.27:5005';
+const url = baseUrl + '/dynamicArt/getEmotionPercentages/';
 
 window.onload = function() {
-    const baseUrl = "http://3.15.139.27:5005";
+  const canvas = document.getElementById('word-cloud');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    const canvas = document.getElementById("word-cloud");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
 
-    if (canvas.getContext) {
-        const ctx = canvas.getContext("2d");
+    setInterval(() => {
+      fetchData().then(emotionData => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        fetchData().then(emotionData => {
-            createWordCloud(emotionData, ctx);
-        });
-    }
+        const words = Object.keys(emotionData);
+        const maxCount = Math.max(...Object.values(emotionData));
+        const minSize = 30;
+        const maxSize = 80;
+        const height = canvas.height / (words.length + 1);
+        const fontSizeFactor = (maxSize - minSize) / maxCount;
+
+        for (let i = 0; i < words.length; i++) {
+          const word = words[i];
+          const count = emotionData[word];
+          let size = minSize + count * fontSizeFactor;
+          let x = canvas.width / 2;
+          let y = (i + 1) * height;
+          ctx.font = `${size}px Arial`;
+          ctx.fillStyle = getRandomColor();
+          ctx.textAlign = 'center';
+          ctx.fillText(word, x, y);
+        }
+      });
+    }, 1000);
+  }
 };
 
 async function fetchData() {
-    const response = await fetch("http://3.15.139.27:5005/retrieve/words");
-    const data = await response.json();
-    return data.words;
-}
-
-function createWordCloud(emotionData, ctx) {
-    const words = emotionData.map(word => {
-        return {
-            text: word,
-            size: Math.random() * 50 + 20,
-            color: getRandomColor(),
-            x: Math.random() * ctx.canvas.width,
-            y: Math.random() * ctx.canvas.height,
-            speed: Math.random() * 3 + 1
-        };
-    });
-
-    setInterval(() => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        words.forEach(word => {
-            ctx.font = `${word.size}px Arial`;
-            ctx.fillStyle = word.color;
-            ctx.fillText(word.text, word.x, word.y);
-            word.x += word.speed;
-            if (word.x > ctx.canvas.width) {
-                word.x = -ctx.measureText(word.text).width;
-                word.y = Math.random() * ctx.canvas.height;
-            }
-        });
-    }, 24);
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 function getRandomColor() {
-    const colors = ["#648FFF", "#785EF0", "#DC267F", "#FE6100", "#FFB000"];
-    return colors[Math.floor(Math.random() * colors.length)];
+  const colors = ['#648FFF', '#785EF0', '#DC267F', '#FE6100', '#FFB000'];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
