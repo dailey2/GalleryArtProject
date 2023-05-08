5059572ed5363d2e947df95a5f27e9c3c072d8b5
Connor <dailey2@kenyon.edu>
2023-05-08 21:56:47 +0000

    final

--- dynamicArt.cpp
+++ dynamicArt.cpp
@@ -10,6 +10,7 @@
 #include "dynamicArtDB.h"
 #include "dynamicArtInfo.h"
 
+
 using namespace httplib;
 using namespace std;
 using json = nlohmann::json;
@@ -19,23 +20,19 @@ const int port = 5005;
 dynamicArtDB obj; // Create a global instance of the dynamicArtDB class
 
 int main() {
-	httplib::Server svr;
-    // Vector to store applicable emotions
-	vector<std::string> emotions = {"Happiness", "Sadness", "Fear", "Disgust", "Anger", "Surprise"};
-	
-	// API to get the percentages of each emotion from the responses database
-	svr.Get(R"(/dynamicArt/getEmotionPercentages/)", [&](const Request& req, Response& res) {
-		res.set_header("Access-Control-Allow-Origin", "*");
-		json result;
+    httplib::Server svr;
 
-		for (const auto& e : emotions) {
-			int count = obj.getEmotionCount(e);
-			result[e] = count;
-		}
-		res.set_content(result.dump(), "application/json");
-	});
+    svr.Get(R"(/dynamicArt/getEmotionPercentages/)", [&](const Request& req, Response& res) {
+        res.set_header("Access-Control-Allow-Origin", "*");
+          json result;
 
-    svr.listen("localhost", 8080);
+ 
+        result = obj.getEmotionCount();
 
+  
+   res.set_content(result.dump(), "application/json");
+    });
+
+    svr.listen("0.0.0.0", port);
     return 0;
 }
--- dynamicArt.js
+++ dynamicArt.js
@@ -1,107 +1,102 @@
-const baseUrl = 'http://3.15.139.27';
+var baseUrl = 'http://3.15.139.27:5005';
+
 const url = baseUrl + '/dynamicArt/getEmotionPercentages/';
 
+
+
 const colr = {
-  "Happiness": "green",
+  "Happiness": "yellow",
   "Surprise": "pink",
-  "Disgust": "orange",
-  "Fear": "yellow",
+  "Disgust": "green",
+  "Fear": "red",
   "Sadness": "blue",
-  "Anger": "red"
+  "Calm": "white" // corrected the capitalization of "Calm"
 };
 
 window.onload = function() {
   const canvas = document.getElementById('word-cloud');
+
   const w = window.innerWidth;
   const h = window.innerHeight;
+
   canvas.width = w;
   canvas.height = h;
+
   const ctx = canvas.getContext('2d');
 
-  function check() {
-    fetchData().then((emotionData) => {
-      const word_attr = createWordData(emotionData);
-      animation(word_attr);
-    });
-  }
+  fetch(baseUrl+'/dynamicArt/getEmotionPercentages/', {
+    method: 'get'
+  })
+   .then(response => response.json())
+  .then(emotionData => { // corrected the variable name to emotionData
+    const word_attr = createWordData(emotionData);
 
-  function animation(word_attr) {
-    for (let i = 0; i < word_attr.length; i++) {
-      const word = word_attr[i];
-      ctx.font = word.font;
-      ctx.fillStyle = word.color;
-      ctx.fillText(word.text, word.x, word.y);
-      word.width = ctx.measureText(word.text).width;
-      ctx.stroke();
-    }
-    move(word_attr);
-  }
-
-  function move(word_attr) {
-    for (let i = 0; i < word_attr.length; i++) {
-      const word = word_attr[i];
-      if (word.x > w) {
-        word.x = -word.width;
-        word.y = Math.random() * h;
-      } else {
-        word.x += word.speed;
+    function animation() {
+      for (let i = 0; i < word_attr.length; i++) {
+        const word = word_attr[i];
+        ctx.font = word.font;
+        ctx.fillStyle = word.color;
+        ctx.fillText(word.text, word.x, word.y);
+        word.width = ctx.measureText(word.text).width;
+        ctx.stroke();
       }
+      move();
     }
-  }
 
-  setInterval(function() {
-    ctx.clearRect(0, 0, w, h);
-    check();
-  }, 3000);
-
-  function createWordData(emotionData) {
-    const words = [];
-    for (const emotion in emotionData) {
-      const count = emotionData[emotion];
-      for (let i = 0; i < count; i++) {
-        const word = criteria(emotion);
-        if (!overlapping(word.x, word.y, word.width, words)) {
-          words.push(word);
+    function move() {
+      for (let i = 0; i < word_attr.length; i++) {
+        const word = word_attr[i];
+        if (word.x > w) {
+          word.x = -word.width;
+          word.y = Math.random() * h;
+        } else {
+          word.x += word.speed;
         }
       }
     }
-    return words;
-  }
+        setInterval(function() {
+      ctx.clearRect(0, 0, w, h);
+      animation();
+    }, 24);
 
-  function criteria(key) {
-    const word = {};
-    word.text = key;
-    word.x = Math.random() * w;
-    word.y = Math.random() * h;
-    word.font = emotionData[key] * 5 + 'px Arial';
-    word.speed = emotionData[key] / 10;
-    word.color = colr[key];
-    word.width = 0;
-    return word;
-  }
-
-  function overlapping(x, y, size, placedWords) {
-    for (let i = 0; i < placedWords.length; i++) {
-      const word = placedWords[i];
-      const distance = Math.sqrt((x - word.x) ** 2 + (y - word.y) ** 2);
-      if (distance < (size + word.width) / 2) {
-        return true;
+    function createWordData(emotionData) {
+       const total = Object.values(emotionData).reduce((acc, val) => acc + val);
+      const words = [];
+      for (const emotion in emotionData) {
+        const count = emotionData[emotion];
+        for (let i = 0; i < count; i++) {
+          const word = criteria(emotion, total);
+          if (!overlapping(word.x, word.y, word.width, words)) {
+            words.push(word);
+          }
+        }
       }
+      return words;
     }
-    return false;
-  }
+
+    function criteria(key,total) {
+      const word = {};
+      word.text = key;
+      word.x = Math.random() * w;
+      word.y = Math.random() * h;
+      const percentage = emotionData[key]/ total;
+      const count = Math.round(percentage * 100);
+      word.font = count * 2+ 'px Arial';
+      word.color = colr[key];
+      word.width = 0;
+      return word;
+         return word;
+    }
+
+    function overlapping(x, y, size, placedWords) {
+      for (let i = 0; i < placedWords.length; i++) {
+        const word = placedWords[i];
+        const distance = Math.sqrt((x - word.x) ** 2 + (y - word.y) ** 2);
+        if (distance < (size + word.width) / 2) {
+          return true;
+        }
+      }
+      return false;
+    }
+  });
 };
-
-async function fetchData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
-
-
-
-
-function getRandomColor() {
-  const colors = ['#648FFF', '#785EF0', '#DC267F', '#FE6100', '#FFB000'];
-  return colors[Math.floor(Math.random() * colors.length)];
-}
